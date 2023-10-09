0d0d0b48c60680b6089953e9cedbd86c51b95872
cesar eduardo patarroyo cortes <cpatarroyo@asesoftware.com>
2023-10-08 20:25:09 -0500

    merge clase de heroes

--- Problema3/ClonPersonajeVideoJuego/Services/FabricaHeroe.cs
+++ Problema3/ClonPersonajeVideoJuego/Services/FabricaHeroe.cs
@@ -1,4 +1,5 @@
-﻿using ClonPersonajeVideoJuego.Models;
+﻿using ClonPersonajeVideoJuego.ClasesDeHeroes;
+using ClonPersonajeVideoJuego.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
